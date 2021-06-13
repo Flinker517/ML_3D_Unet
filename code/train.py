47cdbdbf7d003fe7f330bf57ee9f61aa1c70b930
Flinker517 <68503432+Flinker517@users.noreply.github.com>
2021-06-13 23:42:48 +0800

    Add files via upload

---
+++ code/train.py
@@ -0,0 +1,68 @@
+import argparse
+from functools import partial
+import torch
+import torch.nn as nn
+from fastai.basic_train import Learner
+from fastai.train import ShowGraph
+from fastai.data_block import DataBunch
+from torch import optim
+
+from data import FracNetTrainDataset,Window,MinMaxNorm
+from model.metrics import dice, recall, precision, fbeta_score
+from model.unet import UNet
+# from model.myunet import my3dunet
+from model.unet2 import my3dunet
+from model.losses import MixLoss, DiceLoss
+
+
+def main(args):
+    train_image_dir = args.train_image_dir
+    train_label_dir = args.train_label_dir
+    val_image_dir = args.val_image_dir
+    val_label_dir = args.val_label_dir
+
+    batch_size = 4
+    num_workers = 4
+    optimizer = optim.SGD
+    criterion = MixLoss(nn.BCEWithLogitsLoss(), 0.5, DiceLoss(), 1)
+    # criterion = MixLoss(DiceLoss(), 1)
+
+    thresh = 0.1
+    recall_partial = partial(recall, thresh=thresh)
+    precision_partial = partial(precision, thresh=thresh)
+    fbeta_score_partial = partial(fbeta_score, thresh=thresh)
+
+    model = UNet(1, 1, first_out_channels=16)
+    model = nn.DataParallel(model.cuda())
+    # model = my3dunet(1,1)
+    # model = nn.DataParallel(model.cuda())
+
+    transforms = [Window(-200, 1000), MinMaxNorm(-200, 1000)]
+    ds_train = FracNetTrainDataset(train_image_dir, train_label_dir, transforms=transforms)
+    dl_train = FracNetTrainDataset.get_dataloader(ds_train, batch_size, False, num_workers)
+    ds_val = FracNetTrainDataset(val_image_dir, val_label_dir, transforms=transforms)
+    dl_val = FracNetTrainDataset.get_dataloader(ds_val, batch_size, False, num_workers)
+
+    databunch = DataBunch(dl_train, dl_val, collate_fn=FracNetTrainDataset.collate_fn)
+    learn = Learner(databunch, model, opt_func=optimizer, loss_func=criterion, metrics=[dice, recall_partial, precision_partial, fbeta_score_partial])
+    learn.fit_one_cycle(10, 1e-1, pct_start=0, div_factor=1000, callbacks=[ShowGraph(learn),])
+
+    if args.save_model:
+        torch.save(model.module.state_dict(), "./model0.pth")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train_image_dir", required=True,
+        help="The training image nii directory.")
+    parser.add_argument("--train_label_dir", required=True,
+        help="The training label nii directory.")
+    parser.add_argument("--val_image_dir", required=True,
+        help="The validation image nii directory.")
+    parser.add_argument("--val_label_dir", required=True,
+        help="The validation label nii directory.")
+    parser.add_argument("--save_model", default=False,
+        help="Whether to save the trained model.")
+    args = parser.parse_args()
+
+    main(args)
