ecc36d7cf2e6ea13d46e2bd80d22c765dc264ad3
Flinker517 <68503432+Flinker517@users.noreply.github.com>
2021-06-13 23:42:00 +0800

    Add files via upload

---
+++ code/model/losses.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+__all__ = ['MixLoss', 'DiceLoss']
+
+
+class MixLoss(nn.Module):
+    def __init__(self, *args):
+        super().__init__()
+        self.args = args
+
+    def forward(self, x, y):
+        lf, lfw = [], []
+        for i, v in enumerate(self.args):
+            if i % 2 == 0:
+                lf.append(v)
+            else:
+                lfw.append(v)
+        mx = sum([w * l(x, y) for l, w in zip(lf, lfw)])
+        return mx
+
+
+class DiceLoss(nn.Module):
+    def __init__(self, image=False):
+        super().__init__()
+        self.image = image
+
+    def forward(self, x, y):
+        x = x.sigmoid()
+        i, u = [t.flatten(1).sum(1) if self.image else t.sum() for t in [x * y, x + y]]
+
+        dc = (2 * i + 1) / (u + 1)
+        dc = 1 - dc.mean()
+        return dc
+
